3566f28ff2f51e353f3bc308aeaa0fbea58270e9
unknown <ananyanagar32@gmail.com>
2024-12-25 15:22:28 +0530

    Initial commit

---
+++ src/main/java/com/sheerobot/sheerobot/SheerobotApplication.java
@@ -0,0 +1,13 @@
+package com.sheerobot.sheerobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheerobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheerobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sheerobot/sheerobot/SheerobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheerobot.sheerobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheerobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
